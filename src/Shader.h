f7cc243377f70c0678061bb9df8a912a09c3b27f
rodrigoorozco <rodrigoorozco025@gmail.com>
2024-06-26 19:06:08 -0500

    textures

--- src/Shader.h
+++ src/Shader.h
@@ -14,7 +14,7 @@ class Shader
     public: // private:
         std::string m_FilePath;
         unsigned int m_RendererID;
-        std::unordered_map<std::string, unsigned int> m_UniformLocationCache;
+        std::unordered_map<std::string, int> m_UniformLocationCache;
 
     public:
         Shader(const std::string& filepath);
@@ -24,6 +24,7 @@ class Shader
         void Unbind() const;
 
         // Set Uniforms
+        void SetUniform1i(const std::string& name, int value);
         void SetUniform1f(const std::string& name, float value);
         void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
     
@@ -31,5 +32,5 @@ class Shader
         ShaderProgramSource ParseShader(const std::string& filepath);
         unsigned int CompileShader(unsigned int type, const std::string& source);
         unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
-        unsigned int GetUniformLocation(const std::string& name);
+        int GetUniformLocation(const std::string& name);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
 
 int main(void)
 {
@@ -48,10 +49,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     float positions[] = {
-        -0.5f, -0.5f,
-         0.5f, -0.5f,
-         0.5f,  0.5f,
-        -0.5f,  0.5f,
+        -0.5f, -0.5f, 0.0f, 0.0f, //0
+         0.5f, -0.5f, 1.0f, 0.0f, //1
+         0.5f,  0.5f, 1.0f, 1.0f, //2
+        -0.5f,  0.5f, 0.0f, 1.0f, //3
     };
 
     unsigned int indices[] = {
@@ -59,14 +60,21 @@ int main(void)
         2, 3, 0
     };
 
+    GLCall(glEnable(GL_BLEND));
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+//
     unsigned int vao;
     GLCall(glGenVertexArrays(1, &vao));
     GLCall(glBindVertexArray(vao));
+//
+
 
     VertexArray va;
-    VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+    VertexBuffer vb(positions, 4 * 4 * sizeof(float));
 
     VertexBufferLayout layout;
+    layout.Push<float>(2);
     layout.Push<float>(2);
     va.AddBuffer(vb, layout);
 
@@ -75,6 +83,10 @@ int main(void)
     Shader shader("../res/shaders/Basic.shader");
     shader.Bind();
     shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+    Texture texture("../res/textures/image.gif");
+    texture.Bind();
+    shader.SetUniform1i("u_Texture", 0);
 
     va.Unbind();
     vb.Unbind();
